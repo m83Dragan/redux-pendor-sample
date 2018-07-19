fb682cb6690b2eac76a5f9af8b79c125794f75b6
m83Dragan <sergioxyzribeiro@gmail.com>
2018-07-19 19:27:23 +0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as counterActions from './modules/counter';
-
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as counterActions from "./modules/counter";
+import * as postActions from "./modules/post";
 
 class App extends Component {
-    render() {
-        const { CounterActions, number } = this.props;
+  componentDidMount() {
+    const { number } = this.props;
+    this.getPost(number);
+  }
 
-        
-        return (
-            <div>
-                <h1>{number}</h1>
-                <button onClick={CounterActions.increment}>+</button>
-                <button onClick={CounterActions.decrement}>-</button>
-            </div>
-        );
+  componentWillReceiveProps(nextProps) {
+    if (this.props.number !== nextProps.number) {
+      this.getPost(nextProps.number);
     }
+  }
+
+  getPost = async postId => {
+    const { PostActions } = this.props;
+
+    try {
+      await PostActions.getPost(postId);
+    } catch (e) {
+      console.log("Error!");
+    }
+  };
+
+  render() {
+    const { CounterActions, number, post, error, loading } = this.props;
+
+    return (
+      <div>
+        <p>{number}</p>
+        <button onClick={CounterActions.increment}>+</button>
+        <button onClick={CounterActions.decrement}>-</button>
+        {loading && <h2>Loading...</h2>}
+        {error ? (
+          <h1>Error!</h1>
+        ) : (
+          <div>
+            <h1>{post.title}</h1>
+            <p>{post.title}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default connect(
-    (state) => ({
-        number: state.counter
-    }),
-    (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
-    })
+  state => ({
+    number: state.counter,
+    loading: state.pender.pending["GET_POST"],
+    error: state.pender.failure["GET_POST"],
+    post: state.post.data
+  }),
+  dispatch => ({
+    CounterActions: bindActionCreators(counterActions, dispatch),
+    PostActions: bindActionCreators(postActions, dispatch)
+  })
 )(App);
--- src/store.js
+++ src/store.js
@@ -1,6 +1,15 @@
-import { createStore } from 'redux';
-import modules from './modules';
+import { createStore, applyMiddleware } from "redux";
+import modules from "./modules";
 
-const store = createStore(modules)
+import { createLogger } from "redux-logger";
+import penderMiddleware from "redux-pender";
+import ReduxThunk from "redux-thunk";
+
+const logger = createLogger();
+
+const store = createStore(
+  modules,
+  applyMiddleware(logger, ReduxThunk, penderMiddleware())
+);
 
 export default store;
